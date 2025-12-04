c5bcb911592d55b19689f22166a00a4115b4fb42
Sazzadkhan45 <sazzadkhan753@gmail.com>
2025-12-04 23:51:05 +0600

    Themecontext create for dark & light mode

---
+++ src/Providers/ThemeProvider.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { ThemeContext } from "./ThemeContex";
+
+const ThemeProvider = ({ children }) => {
+  // All state
+  const [isDark, setIsDark] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "dark";
+  });
+
+  //
+  useEffect(() => {
+    const theme = isDark ? "dark" : "light";
+
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [isDark]);
+
+  // toggleTheme
+  const toggleTheme = () => {
+    setIsDark((prev) => !prev);
+  };
+
+  // ThemeInfo
+  const themeInfo = {
+    toggleTheme,
+    isDark,
+    setIsDark,
+  };
+
+  return (
+    <ThemeContext.Provider value={themeInfo}>{children}</ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import ThemeProvider from "./Providers/ThemeProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h2>Hello word</h2>
+    <ThemeProvider>
+      <h2>Hello word</h2>
+    </ThemeProvider>
   </StrictMode>
 );
